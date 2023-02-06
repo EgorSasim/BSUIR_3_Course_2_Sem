9bcfc4a678df4f97613121ba6c56cedc51c3d6a7
egor.sasim <egor.sasim@altezza.org>
2023-02-06 16:55:58 +0300

    modify server settings, SPP-1

--- SPP/lab1/server/server.js
+++ SPP/lab1/server/server.js
@@ -11,12 +11,14 @@ const TASKS = [
     taskStatus: "In progress",
     taskDateTime: "2023-05-07T16:00",
     taskFile: "",
+    taskId: 1,
   },
   {
     taskName: "Wash car",
     taskStatus: "Done",
     taskDateTime: "2020-10-07T16:00",
     taskFile: "",
+    taskId: 2,
   },
 ];
 
@@ -35,10 +37,12 @@ app.get("/", (req, res) => {
   res.render(path.join(__dirname, "../client/index.html"));
 });
 
-app.post("/data", (req, res) => {
-  let data = req.body;
-  if (Object.keys(data).length) {
-    TASKS.push(data);
+app.post("/tasks-list", (req, res) => {
+  let task = req.body;
+  if (Object.keys(task).length) {
+    addId(task);
+    console.log(task);
+    TASKS.push(task);
   }
   res.send(JSON.stringify(TASKS));
   res.end();
