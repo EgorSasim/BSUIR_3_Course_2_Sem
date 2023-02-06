eb6f1c28b1d65ef1df55246581b8c07cab3809ba
egor.sasim <egor.sasim@altezza.org>
2023-02-06 16:29:25 +0300

    add tasks server response, SPP-1

--- SPP/lab1/client/script.js
+++ SPP/lab1/client/script.js
@@ -4,8 +4,6 @@ const taskStatus$ = document.querySelector(".task-creator__task-status");
 const taskDateTime$ = document.querySelector(".task-creator__task-date-time");
 const taskFile$ = document.querySelector(".task-creator__task-file");
 
-const taskCreatorForm$ = document.querySelector(".task-creator");
-
 //add button
 const addTask$ = document.querySelector(".task-creator__task-add");
 
@@ -14,28 +12,35 @@ const tasksList$ = document.querySelector(".tasks-list");
 
 const URL = "http://127.0.0.1:3000";
 
+addTask();
+
 addTask$.onclick = () => {
   if (taskName$.value.length === 0) {
     alert("please, write task name before adding it");
   } else {
-    callBack({ name: "gregory" });
+    addTask({
+      taskName: taskName$.value,
+      taskStatus: taskStatus$.options[taskStatus$.selectedIndex].text,
+      taskDateTime: taskDateTime$.value,
+      taskFile: taskFile$.value,
+    });
   }
 };
 
-const drawAddedTask = () => {
+const drawAddedTask = (taskName, taskStatus, taskDateTime, taskFile) => {
   tasksList$.innerHTML += `
     <div class="task">
         <span class="tasks-list__task-name">
-            ${taskName$.value}
+            ${taskName}
         </span>
         <span class="tasks-list__task-status">
-            ${taskStatus$.value}
+            ${taskStatus}
         </span>
         <span class="tasks-list__task-file">
-            ${taskDateTime$.value}
+            ${taskDateTime}
         </span>
         <span class="tasks-list__task-file">
-            ${taskFile$.value}
+            ${taskFile}
         </span>
         <button class="tasks-list__delete-task">
             &times
@@ -44,19 +49,28 @@ const drawAddedTask = () => {
     `;
 };
 
-function callBack(data) {
-  console.log("call callback");
+function addTask(data) {
+  console.log(data);
   const options = {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
   };
+  clearTasksList();
   fetch(URL + "/data", options).then((response) =>
-    console.log(
-      "response: ",
-      response
-        .text()
-        .then((resolve, reject) => console.log("resolve: ", resolve))
+    response.text().then((resolve, reject) =>
+      JSON.parse(resolve).forEach((task) => {
+        drawAddedTask(
+          task.taskName,
+          task.taskStatus,
+          task.taskDateTime,
+          task.taskFile
+        );
+      })
     )
   );
+}
+
+function clearTasksList() {
+  tasksList$.innerHTML = "";
 }
--- SPP/lab1/server/server.js
+++ SPP/lab1/server/server.js
@@ -5,6 +5,21 @@ const bodyParser = require("body-parser");
 
 const PORT = 3000;
 
+const TASKS = [
+  {
+    taskName: "Do homework",
+    taskStatus: "In progress",
+    taskDateTime: "2023-05-07T16:00",
+    taskFile: "",
+  },
+  {
+    taskName: "Wash car",
+    taskStatus: "Done",
+    taskDateTime: "2020-10-07T16:00",
+    taskFile: "",
+  },
+];
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, "../client/")));
@@ -17,13 +32,15 @@ app.use(
 );
 
 app.get("/", (req, res) => {
-  res.render(path.join(__dirname, "../client/index.ejs"));
+  res.render(path.join(__dirname, "../client/index.html"));
 });
 
 app.post("/data", (req, res) => {
   let data = req.body;
-  console.log("body:", req.body);
-  res.send(JSON.stringify(data));
+  if (Object.keys(data).length) {
+    TASKS.push(data);
+  }
+  res.send(JSON.stringify(TASKS));
   res.end();
 });
 
